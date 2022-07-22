2009b91c247ff59311f50f9ab66b0c10c084328e
PriyankaPatil <piyu28patil7@gmai.com>
2022-07-22 22:38:03 +0530

    First COmmit

---
+++ Sorted-List/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sorted_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
